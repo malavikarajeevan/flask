6d7513853016b3a5365d915f85fc8edc0427fff0
malavika <malavikarajeevan85@gmail.com>
2024-02-15 08:53:31 +0530

    new file

--- flasksite/app.py
+++ flasksite/app.py
@@ -1,10 +1,16 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 APP=Flask(__name__)
 
 @APP.route('/')
 def home():
     return render_template('index.html')
+@APP.route('/success',methods=['POST','GET'])
+def success():
+    if request.method=='POST':
+        result=request.form
+        print(result)
+    return render_template('success.html',result=result)
 
 
 APP.run(debug=True)
